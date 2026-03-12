3b693d03df17d427ac8682ac2bce5ec00af7bf42
Md. Motiur Rahman <motiur0883@gmail.com>
2026-03-12 23:20:47 +0600

    Added Bit String solution
    "Solutions for CSES Problem Set (Bit String) in C++"

---
+++ Bitstrings.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    const long long MOD = 1000000007;
+    long long ans = 1;
+
+    for(int i = 0; i < n; i++){
+        ans = (ans * 2) % MOD;
+    }
+
+    cout << ans;
+}
