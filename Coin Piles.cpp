c892356b0cc989ca1ca623a2c6af510ff3055600
Md. Motiur Rahman <motiur0883@gmail.com>
2026-03-14 14:55:12 +0600

    Implement Coin Piles problem solution

---
+++ Coin Piles.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        long long a, b;
+        cin >> a >> b;
+
+        if((a + b) % 3 == 0 && max(a,b) <= 2 * min(a,b))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+}
