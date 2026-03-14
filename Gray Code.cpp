e4f25495524fe7b482237d1e297bba72ecef5c29
Md. Motiur Rahman <motiur0883@gmail.com>
2026-03-14 19:24:12 +0600

    Implement Gray Code generation in C++

---
+++ Gray Code.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<string> gray;
+
+    gray.push_back("0");
+    gray.push_back("1");
+
+    for(int i=2;i<=n;i++){
+        int size = gray.size();
+
+        for(int j=size-1;j>=0;j--){
+            gray.push_back(gray[j]);
+        }
+
+        for(int j=0;j<size;j++){
+            gray[j] = "0" + gray[j];
+        }
+
+        for(int j=size;j<2*size;j++){
+            gray[j] = "1" + gray[j];
+        }
+    }
+
+    for(string s : gray){
+        cout << s << endl;
+    }
+}
