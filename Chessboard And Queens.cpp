f23fbe7b80b10e69658427db7ec5796306a190ba
Md. Motiur Rahman <motiur0883@gmail.com>
2026-03-16 22:08:06 +0600

    Implement N-Queens problem solution

---
+++ Chessboard And Queens.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string board[8];
+int ans = 0;
+
+bool col[8], diag1[15], diag2[15];
+
+void solve(int r){
+
+    if(r == 8){
+        ans++;
+        return;
+    }
+
+    for(int c=0;c<8;c++){
+
+        if(board[r][c]=='*') continue;
+
+        if(col[c] || diag1[r+c] || diag2[r-c+7]) continue;
+
+        col[c] = diag1[r+c] = diag2[r-c+7] = true;
+
+        solve(r+1);
+
+        col[c] = diag1[r+c] = diag2[r-c+7] = false;
+    }
+}
+
+int main(){
+
+    for(int i=0;i<8;i++)
+        cin >> board[i];
+
+    solve(0);
+
+    cout << ans;
+}
